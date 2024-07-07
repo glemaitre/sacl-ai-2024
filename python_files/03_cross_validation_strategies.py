ae0ae211583436fd306a50e089812dd9fff16d54
Guillaume Lemaitre <guillaume@probabl.ai>
2024-07-08 01:03:48 +0200

    add cross-validation narrative

--- python_files/03_cross_validation_strategies.py
+++ python_files/03_cross_validation_strategies.py
@@ -1,3 +1,15 @@
+# %% [markdown]
+#
+# # Cross-validation strategies
+#
+# The previous notebooks introduced how to evaluate a model and how to create a
+# specific preprocessing pipeline depending of the last model.
+#
+# In this notebook, we will check a bit more some details regarding the cross-validation
+# strategies and some of the pitfalls that we can encounter.
+#
+# Let's take iris dataset and evaluate a logistic regression model.
+
 # %%
 from sklearn.datasets import load_iris
 
@@ -28,6 +40,11 @@ cv_results = cross_validate(
 cv_results = pd.DataFrame(cv_results)
 cv_results[["train_score", "test_score"]]
 
+# %% [markdown]
+#
+# We observe that the training score is always zero that is really surprising. We can
+# check the target to understand why.
+
 # %%
 ax = target.plot()
 _ = ax.set(
@@ -35,6 +52,12 @@ _ = ax.set(
     ylabel="Target value",
     title="Iris dataset target values",
 )
+
+# %% [markdown]
+#
+# We observe that the data is ordered by target. This is a problem because the KFold
+# object is not shuffling the data before splitting it. Therefore, we always get a
+# test set that does not contain a class seen during `fit`.
 
 # %%
 for cv_fold_idx, (train_indices, test_indices) in enumerate(cv.split(df, target)):
@@ -47,6 +70,12 @@ for cv_fold_idx, (train_indices, test_indices) in enumerate(cv.split(df, target)
         f"Class counts on the test set:\n" f"{target.iloc[test_indices].value_counts()}"
     )
     print()
+
+# %% [markdown]
+#
+# We can use a `StratifiedKFold` object to ensure that the class distribution is
+# preserved in each fold. A side effect will be that all classes will be present in the
+# training set and testing set.
 
 # %%
 from sklearn.model_selection import StratifiedKFold
@@ -70,6 +99,11 @@ for cv_fold_idx, (train_indices, test_indices) in enumerate(cv.split(df, target)
     )
     print()
 
+# %% [markdown]
+#
+# This is particularly useful when we have imbalanced classes. Let's check the class
+# distribution of the breast cancer dataset.
+
 # %%
 from sklearn.datasets import load_breast_cancer
 
@@ -77,6 +111,11 @@ df, target = load_breast_cancer(as_frame=True, return_X_y=True)
 
 # %%
 target.value_counts(normalize=True)
+
+# %% [markdown]
+#
+# Here, we see that the proportion of the two classes is not equal. We can check the
+# class distribution in each fold using a `KFold` object.
 
 # %%
 cv = KFold(n_splits=3, shuffle=True, random_state=0)
@@ -92,6 +131,12 @@ for cv_fold_idx, (train_indices, test_indices) in enumerate(cv.split(df, target)
     )
     print()
 
+# %% [markdown]
+#
+# We observe that the class distribution is not preserved in each fold. We can use a
+# `StratifiedKFold` object to ensure that the class distribution is preserved in each
+# fold.
+
 # %%
 cv = StratifiedKFold(n_splits=3)
 for cv_fold_idx, (train_indices, test_indices) in enumerate(cv.split(df, target)):
@@ -106,18 +151,38 @@ for cv_fold_idx, (train_indices, test_indices) in enumerate(cv.split(df, target)
     )
     print()
 
+# %% [markdown]
+#
+# Now, let's check the documentation of the `cross_validate` function to see if this
+# function was already providing a way to stratify the data.
+
 # %%
 help(cross_validate)
+
+# %% [markdown]
+#
+# Now, we will look at the notion of `groups` in cross-validation. We will use the
+# digits dataset and group the samples by writer.
 
 # %%
 from sklearn.datasets import load_digits
 
 df, target = load_digits(return_X_y=True)
 
+# %% [markdown]
+#
+# We create a simple model that is a logistic regression model with a scaling of the
+# data.
+
 # %%
 from sklearn.preprocessing import MinMaxScaler
 
 logistic_regression = make_pipeline(MinMaxScaler(), LogisticRegression())
+
+# %% [markdown]
+#
+# Let's start to evaluate the model using a `KFold` object, once without shuffling and
+# once with shuffling.
 
 # %%
 cv = KFold(n_splits=13)
@@ -134,6 +199,12 @@ print(
     f"Mean test score: {cv_results['test_score'].mean():.3f} +/- "
     f"{cv_results['test_score'].std():.3f}"
 )
+
+# %% [markdown]
+#
+# Surprisingly, the mean test score is increasing when shuffling the data. Let's check
+# if this is due to the random seed.
+
 # %%
 for seed in range(10):
     cv = KFold(n_splits=13, shuffle=True, random_state=seed)
@@ -142,6 +213,16 @@ for seed in range(10):
         f"Mean test score: {cv_results['test_score'].mean():.3f} +/- "
         f"{cv_results['test_score'].std():.3f}"
     )
+
+# %% [markdown]
+#
+# Apparently not. The reason is that the samples are grouped by writer. By shuffling,
+# we are mixing the samples from different writers. Therefore, we are learning a model
+# on some writers that are also used to test. However, if we want to have a model that
+# generalizes well to new writers, we should not mix the samples from the same writer
+# between the training and testing set.
+#
+# Here, we provide a `groups` array that mentioned the writer ID for each sample.
 
 # %%
 from itertools import count
@@ -183,6 +264,11 @@ plt.xlabel("Target index")
 plt.ylabel("Writer index")
 _ = plt.title("Underlying writer groups existing in the target")
 
+# %% [markdown]
+#
+# We can use this information to properly evaluate our model. We need to use the
+# `GroupKFold` object and pass the `groups` parameter to the `cross_validate` function.
+
 # %%
 from sklearn.model_selection import GroupKFold
 
@@ -193,4 +279,7 @@ print(
     f"{cv_results['test_score'].std():.3f}"
 )
 
-# %%
+# %% [markdown]
+#
+# We observe that the mean test score is even lower but certainly closer to the true
+# performance of the model.
