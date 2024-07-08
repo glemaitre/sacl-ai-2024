9038a43678e19c4243ce0d77bcc41600d54b8a8c
Guillaume Lemaitre <guillaume@probabl.ai>
2024-07-08 10:25:30 +0200

    iter

--- python_files/05_nested_cross_validation.py
+++ python_files/05_nested_cross_validation.py
@@ -1,3 +1,13 @@
+# %% [markdown]
+#
+# # Hyperparameter tuning with nested cross-validation
+#
+# In this notebook, we will illustrate how to perform hyperparameter tuning
+# using nested cross-validation.
+#
+# We start be defining the predictive model that we created in the previous
+# notebook.
+
 # %%
 import pandas as pd
 
@@ -27,6 +37,10 @@ from sklearn.linear_model import LogisticRegression
 logistic_regression = tabular_learner(LogisticRegression())
 logistic_regression
 
+# %% [markdown]
+#
+# In the past, we saw how to evaluate this model with cross-validation.
+
 # %%
 import pandas as pd
 from sklearn.model_selection import cross_validate
@@ -35,8 +49,17 @@ cv_results = cross_validate(logistic_regression, X, y, cv=5, return_train_score=
 cv_results = pd.DataFrame(cv_results)
 cv_results[["train_score", "test_score"]]
 
+# %% [markdown]
+#
+# However, this machine learning pipeline has some hyperparameters that are set to
+# default values and not necessarily optimal for our problem.
+
 # %%
 logistic_regression.get_params()
+
+# %% [markdown]
+#
+# We can tune these hyperparameters using a search strategy.
 
 # %%
 from scipy.stats import loguniform
@@ -56,13 +79,25 @@ tuned_model = RandomizedSearchCV(
 tuned_model
 
 # %%
+# set `return_estimator=True` to access the best model found during the search
 cv_results = cross_validate(tuned_model, X, y, cv=5, return_estimator=True)
 cv_results = pd.DataFrame(cv_results)
 cv_results
 
+# %% [markdown]
+#
+# We can check the best estimator that have been for each fold.
+
 # %%
 for estimator in cv_results["estimator"]:
     print(estimator.best_params_)
+
+# %% [markdown]
+#
+# However, the previous approach is not the best way to tune hyperparameters.
+# We used the default score that is the accuracy here. However, if we have a
+# probabilistic model, we should instead optimized a proper scoring function instead
+# of a threshold-based metric. So we can optimize the log loss instead of the accuracy.
 
 # %%
 cv_results = cross_validate(
